0860902740ef7b87f86f08f8f27d387ebcd37ad1
Ganetsky <a.ganetsky@ganetskya-l.itransition.corp>
2018-03-26 14:03:15 +0300

    safe commit

--- mysql/sql.sh
+++ mysql/sql.sh
@@ -3,5 +3,4 @@ export DEBIAN_FRONTEND=noninteractive
 
 MYSQL_ROOT_PASSWORD='root'
 echo debconf mysql-server/root_password password $MYSQL_ROOT_PASSWORD | debconf-set-selections
-echo debconf mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD |  debconf-set-selections
-
+echo debconf mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD | debconf-set-selections
